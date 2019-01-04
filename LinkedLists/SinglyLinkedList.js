724f8c0797bd6c247acb46079e8ad0b390d1b459
iKurisu <chris.epic01@gmail.com>
2019-01-04 14:35:47 +0100

    Added delete, toArray and size to SinglyLinkedList

--- LinkedLists/SinglyLinkedList.js
+++ LinkedLists/SinglyLinkedList.js
@@ -16,6 +16,40 @@ class SinglyLinkedList {
       node.next = new SinglyLinkedListNode(value);
     }
   }
+
+  delete() {
+    let node = this.head;
+
+    if (!node) {
+      return false;
+    } else if (!node.next) {
+      this.head = null;   
+    } else {
+      while (node.next.next) {
+        node = node.next;
+      }
+
+      node.next = null;
+    }
+  }
+
+  toArray() {
+    let node = this.head;
+    if (!node) return [];
+
+    const array = [node.data];
+
+    while (node.next) {
+      node = node.next;
+      array.push(node.data);
+    }
+
+    return array;
+  }
+
+  get size() {
+    return this.toArray().length;
+  }
 }
 
 class SinglyLinkedListNode {

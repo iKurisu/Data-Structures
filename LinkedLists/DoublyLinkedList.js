329d38f306813e0df61b0fbd19aa4f9ac64dee6b
iKurisu <chris.epic01@gmail.com>
2019-01-08 09:16:23 +0100

    Added methods to DoublyLinkedList

--- LinkedLists/DoublyLinkedList.js
+++ LinkedLists/DoublyLinkedList.js
@@ -8,13 +8,50 @@ class DoubleLinkedList {
       this.head = new DoubleLinkedListNode(value);
     } else {
       let node = this.head;
-      while (node.next) {
-        node = node.next;
-      }
       
+      while (node.next) node = node.next;
+  
       node.next = new DoubleLinkedListNode(value);
       node.next.previous = node;
     }
+  }
+
+  pop() {
+    let node = this.head;
+
+    if (!node) {
+      return false;
+    } else if (!node.next) {
+      this.head = null;
+    } else {
+      while (node.next.next) node = node.next;
+
+      const nodeToReturn = node.next;
+      nodeToReturn.previous = null;
+      
+      node.next = null; 
+
+      return nodeToReturn;
+    }
+  }
+
+  toArray() {
+    let node = this.head;
+
+    if (!node) return [];
+
+    const array = [node.data];
+
+    while (node.next) {
+      node = node.next;
+      array.push(node.data);
+    }
+
+    return array;
+  }
+
+  get size() {
+    return this.toArray().length;
   }
 }
 
@@ -25,3 +62,13 @@ class DoubleLinkedListNode {
     this.previous = null;
   }
 }
+
+const list = new DoubleLinkedList();
+list.insert(5);
+list.insert(2);
+list.insert(3);
+const lastNode = list.pop();
+list.insert(5)
+
+console.log(list.size);
+
